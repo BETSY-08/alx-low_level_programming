8b81a68f31735944db432b0e609273ee0f29474c
BETSY-08 <ntinyari.b.g@gmail.com>
2022-03-10 22:14:00 +0000

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
  * main - this is the entry point
- * Return; always 0 (success)
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
