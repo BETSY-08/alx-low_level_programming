b582b77e507eaea516149cbc09381d982f0a7b93
BETSY-08 <ntinyari.b.g@gmail.com>
2022-03-10 22:04:00 +0000

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main entry point
- * Return; always 0 (success)
+ * main - this is the entry point
+ * Return ; always 0 (success)
  */
 
-int main (void)
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts(""Programming is like building a multilingual puzzle\n");
 	return 0;
 }
