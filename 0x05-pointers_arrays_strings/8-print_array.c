85bf12d5a7a61fd8e285aa6d5b1d609194052be6
BETSY-08 <ntinyari.b.g@gmail.com>
2022-03-21 23:28:35 +0000

    rints n elements of an array of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,16 +1,22 @@
 #include "main.h"
-#include "2-strlen.c"
 
 /**
-* print_rev - prints a string, in reverse, followed by a new line.
-* @s: string to print
-* Return: string reversed
-*/
-
-void print_rev(char *s)
+ * print_array -rints n elements of an array of integers
+ * @a: array to be printed
+ * @n: number of elements to print
+ */
+void print_array(int *a, int n)
 {
-int len;
-for (len = _strlen(s) - 1; len >= 0; len--)
-_putchar(s[len]);
-_putchar(10);
+int k;
+
+for (k = 0; k < n; k++)
+{
+if (k == 0)
+printf("%d", a[k]);
+else
+printf(", %d", a[k]);
 }
+printf("\n");
+}
+
+
