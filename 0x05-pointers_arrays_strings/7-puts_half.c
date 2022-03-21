c9cf0a4680c833cff1bab2be02eb6f12de2e9e3a
BETSY-08 <ntinyari.b.g@gmail.com>
2022-03-21 23:11:21 +0000

    half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+* puts_half - inputs
+* @str: string to print
+*/
+
+void puts_half(char *str)
+{
+int m = 0;
+int k = 0;
+if (_strlen(str) % 2 != 0)
+{
+k += 1;
+}
+for (m = (_strlen(str) + k) / 2; m < _strlen(str); m++)
+_putchar(*(str + m);
+_putchar('\n');
+}
