accc652fc4072c881ed1521af28a7ef803ef7705
BETSY-08 <ntinyari.b.g@gmail.com>
2022-03-21 19:36:44 +0000

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - function that takes a pointer to an int
+* updates value to 98
+* @n: pointer to the variable being updated
+*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
