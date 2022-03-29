a97b9ee3ef22c409296a0ee892ee3cce909880b7
BETSY-08 <ntinyari.b.g@gmail.com>
2022-03-29 11:02:56 +0000

    copies a string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -10,11 +10,11 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int b;
+	int b = 0;
 
 for (b = 0; b < n && src[b] != '\0'; b++)
 	dest[b] = src[b];
-for ( ; n < b; b++)
+for ( ; n > b; ++b)
 	dest[b] = '\0';
 
 return (dest);
