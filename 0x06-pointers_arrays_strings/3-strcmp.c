5f248297ef994f97b6c444a26fda6b0f48d12e8c
BETSY-08 <ntinyari.b.g@gmail.com>
2022-03-29 10:51:38 +0000

    comparing two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _strcmp - comparing two strings
+  * @s1: first string
+  * @s2: second string
+  * Return: 0
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+while (*s1)
+{
+	if (*s1 != *s2)
+		break;
+	s1++;
+	s2++;
+}
+return (*(const unsigned char *)s1 - *(const unsigned char *)s2);
+}
+
