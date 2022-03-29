64ba716e93903d7f57fc6f835635f0982e779dbb
BETSY-08 <ntinyari.b.g@gmail.com>
2022-03-29 14:09:36 +0000

    capitalize all words of a string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * *cap_string - capitalize all words of a string
+  *@str: input string
+  *Return: string in caps
+  */
+
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		if (i == 0; str[i] >= 'a' && str[i] <= 'z')
+		{
+			str[i] = str[i] - 32;
+		}
+		if (string_separators(str[i]) && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+		{
+			str[i] = str[i + 1] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
